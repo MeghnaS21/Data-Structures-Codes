6dad29c7877082a11025db36af810a1438b59db8
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2021-10-19 14:08:34 +0530

    added binary search tree code

---
+++ binary_search_tree(search_node).cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class BinaryTrees{
+    public:
+    T data;
+    BinaryTrees* left;
+    BinaryTrees* right;
+
+    BinaryTrees(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+BinaryTrees<int>* takeInputLevelWise()
+{
+    int rootData;
+    cout<<"Enter root data"<<endl;
+    cin>>rootData;
+
+    if(rootData==-1)
+      return NULL;
+
+    BinaryTrees<int>* root = new BinaryTrees<int> (rootData);
+
+    queue<BinaryTrees<int>*> pendingNodes;
+    pendingNodes.push(root);
+
+    while(pendingNodes.size()!=0)
+    {
+        BinaryTrees<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+
+        cout<<"Enter left child"<<front->data<<endl;
+        int leftCHildData;
+        cin>>leftCHildData;
+
+        if(leftCHildData != -1){
+            BinaryTrees<int> * child = new BinaryTrees<int>(leftCHildData);
+            front->left = child;
+
+            pendingNodes.push(child);
+        }
+        cout<<"Enter right child"<<front->data<<endl;
+        int rightCHildData;
+        cin>>rightCHildData;
+
+        if(rightCHildData != -1){
+            BinaryTrees<int> * child = new BinaryTrees<int>(rightCHildData);
+            front->right = child;
+
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+BinaryTrees<int>* search(BinaryTrees<int>* root, int s){
+    if(root == NULL)
+      return NULL;
+    
+    if(root->data == s) 
+      return root;
+    
+    if(root->data < s)
+      return search(root->right, s);
+    else
+      return  search(root->left, s);
+}
+
+void PrintNode(BinaryTrees<int>* root){
+     if(root==NULL)
+       return;
+
+    cout<<root->data<<": ";
+
+    if(root->left!=NULL)
+    {
+        cout<<"L"<<root->left->data;
+    }
+
+    if(root->right!=NULL)
+    {
+        cout<<"R"<<root->right->data;
+    }
+    cout<<endl;
+    PrintNode(root->left);
+    PrintNode(root->right);
+}
+
+
+
+
+int main(){
+
+    BinaryTrees<int>* root = takeInputLevelWise();
+    PrintNode(root);
+    BinaryTrees<int>* node = search(root, 9);
+    cout<<node->data;
+}
