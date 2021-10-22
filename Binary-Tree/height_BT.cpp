11f4c0859b781e2bb97953079b9a60ef4ff3e096
Meghna Srivastava <50775297+MeghnaS21@users.noreply.github.com>
2021-10-22 08:43:28 +0530

    Added height of BT code

---
+++ Binary-Tree/height_BT.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+template<typename T>
+class binarytreenode{
+    public:
+    T data;
+    binarytreenode* left;
+    binarytreenode* right;
+
+    binarytreenode(T data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+int height(binarytreenode<int>* root){
+    if(root==NULL)
+      return 0;
+
+    return 1+max(height(root->left), height(root->right));
+}
+binarytreenode<int>* takeinput()
+{
+    int rootdata;
+    cin>>rootdata;
+    binarytreenode<int>* root = new binarytreenode<int>(rootdata);
+    queue<binarytreenode<int>*> pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0){
+        binarytreenode<int>* front = pendingnodes.front();
+        pendingnodes.pop();
+        int leftdata;
+        cin>>leftdata;
+        if(leftdata!=-1){
+            binarytreenode<int>* leftchild = new binarytreenode<int>(leftdata);
+            front->left = leftchild;
+            pendingnodes.push(leftchild);
+        }
+        int rightdata;
+        cin>>rightdata;
+        if(rightdata!=-1){
+            binarytreenode<int>* rightchild = new binarytreenode<int>(rightdata);
+            front->right = rightchild;
+            pendingnodes.push(rightchild);
+        }
+    }
+    return root;
+}
+int main() {
+    binarytreenode<int>* root = takeinput();
+    cout<<height(root);
+
+    
+}
